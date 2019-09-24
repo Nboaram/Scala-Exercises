43b886ba501d7bda36178ec10a5fd2139abf1edf
nboaram <nboaram1@hotmail.co.uk>
2019-09-24 16:00:01 +0100

    iteration 2 recursion

--- src/main/scala/Day1/IteratorsRecursion.scala
+++ src/main/scala/Day1/IteratorsRecursion.scala
@@ -5,18 +5,40 @@ object IteratorsRecursion {
   def main(args: Array[String]): Unit = {
 
     simpleIterator("Yo", 3)
+    rectangleIterator("HA", 5)
   }
-   def simpleIterator(text: String, number : Int): Unit = {
-     if(number == 1){
 
-     }
-     else simpleIterator(text, number - 1)
-     println(text + number)
-   }
+  def simpleIterator(text: String, number: Int): Unit = {
 
-  def rectangleIterator (text: String, number : Int): Unit = {
-    if (number == 1){
+    if (number == 1) {
+    }
+    else simpleIterator(text, number - 1)
+    println(text + number)
+  }
+
+  def rectangleIterator(text: String, number: Int): Unit = {
+
+    textColumnIterator(text, number, number)
+  }
+
+  def textLineIterator(text: String, number: Int): Unit = {
+
+    if (number == 1) {
       println(text)
+    }
+    else {
+      print(text)
+      textLineIterator(text, number - 1)
+    }
+  }
+
+  def textColumnIterator(text: String, colNumber: Int, width: Int): Unit = {
+
+    if (colNumber == 1) {
+    }
+    else {
+      textLineIterator(text, width)
+      textColumnIterator(text, colNumber - 1, width)
     }
   }
 }
