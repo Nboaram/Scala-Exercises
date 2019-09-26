f0f1279382f2693a4a23e4eddc52e802d94fd9a3
nboaram <nboaram1@hotmail.co.uk>
2019-09-26 09:16:16 +0100

    Fixed Files

--- src/main/scala/Day2/SwapNumbers.scala
+++ src/main/scala/Day2/SwapNumbers.scala
@@ -3,12 +3,18 @@ package Day2
 object SwapNumbers {
 
   def main(args: Array[String]): Unit = {
-
+    swapNumbers(List(1,3))
+    swapNumbers(Array(1,3))
+    swapNumbers((1,3))
+    swapNumbers("Three")
   }
 
   def swapNumbers[T](entry: T): Unit = {
     entry match {
+
       case entry : List[Int] => println(entry.reverse)
+      case entry : Array[Int] => for (i <- entry.length to 1 by -1) {print(entry{i-1})}
+      case (a,b) => println(b,a)
       case _ => "Nothing Recognised"
     }
   }
